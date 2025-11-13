288430dfa5803197f776621201ae26f5b2883ed7
itsbrunodev <brunka215@gmail.com>
2025-11-14 00:02:03 +0100

    feat: show thesis image in rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://erettsegi-tetelek.itsbruno.dev",
+  site:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4321"
+      : "https://erettsegi-tetelek.itsbruno.dev",
   integrations: [mdx(), sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,13 +2,32 @@ import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 
 import { CONFIG } from "../lib/constants";
+import { getImage } from "astro:assets";
 
 export async function GET(context) {
-  const magyar = await getCollection("magyar");
-  const matematika = await getCollection("matematika");
-  const tortenelem = await getCollection("tortenelem");
+  const [magyar, matematika, tortenelem] = await Promise.all([
+    getCollection("magyar"),
+    getCollection("matematika"),
+    getCollection("tortenelem"),
+  ]);
 
-  const theses = [...magyar, ...matematika, ...tortenelem];
+  const theses = [...magyar, ...matematika, ...tortenelem].map(
+    (thesis) =>
+      new Promise((resolve, reject) => {
+        getImage({
+          src: import(`../assets/${thesis.collection}/${thesis.id}/cover.jpg`),
+        })
+          .then((image) =>
+            resolve({
+              ...thesis,
+              image: new URL(image.src, context.site).toString(),
+            }),
+          )
+          .catch(reject);
+      }),
+  );
+
+  const thesesWithImages = await Promise.all(theses);
 
   const { title, description } = CONFIG.site;
 
@@ -16,9 +35,10 @@ export async function GET(context) {
     title,
     description,
     site: context.site,
-    items: theses.map((thesis) => ({
+    items: thesesWithImages.map((thesis) => ({
       ...thesis.data,
       link: `/${thesis.collection}/${thesis.id}`,
+      customData: `<enclosure url="${thesis.image}" type="image/webp" />`,
     })),
   });
 }
