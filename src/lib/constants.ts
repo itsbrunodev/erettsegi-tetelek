f929bc8a5e9c2d40a5474793fab97a1f638d70d7
itsbrunodev <brunka215@gmail.com>
2025-11-13 22:06:57 +0100

    chore: update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,6 @@ export const CONFIG = {
   site: {
     title: "Érettségi Tételek 2026",
     description:
-      "2026-os kidolgozott érettsegi történelem, magyar és matematika tételek.",
+      "2026-os kidolgozott érettségi történelem, magyar és matematika tételek.",
   },
 };
